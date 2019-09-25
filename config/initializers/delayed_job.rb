1799f54a6e54ddd08822f3f5a0d335ed0e7a46e7
cob16 <public+github@cormacbrady.info>
2019-09-25 17:01:54 +0100

    disable logging all args in workers
    so sensitive data is not logged

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,1 +1,9 @@
 Delayed::Job.logger = Rails.logger
+
+ActiveSupport.on_load :active_job do
+  class ActiveJob::Logging::LogSubscriber
+    private def args_info(job)
+      ' ### args hidden ###'
+    end
+  end
+end
