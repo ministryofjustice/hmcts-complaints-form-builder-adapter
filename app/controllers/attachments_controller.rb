d96c75629d6b0c50dab3c6fb82d84547e1e2726d
Tom Taylor <tommotaylor@gmail.com>
2019-09-25 11:29:48 +0100

    Serve decrypted attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,15 @@
 class AttachmentsController < ActionController::API
   def show
-    head :ok
+    attachment = Attachment.find_by(identifier: params.fetch(:id))
+
+    decrypted_file_data = Cryptography.new(
+      encryption_key: Base64.strict_decode64(attachment.encryption_key),
+      encryption_iv: Base64.strict_decode64(attachment.encryption_iv)
+    ).decrypt(file: HTTParty.get(attachment.url).body)
+
+    render :ok, body: decrypted_file_data, headers: {
+      'Content-Type' => 'text/plain',
+      'Content-disposition' => 'attachment; filename=test.txt'
+    }
   end
 end
