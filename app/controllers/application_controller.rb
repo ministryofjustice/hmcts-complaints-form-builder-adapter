6dad0998bdabc950046d8b181ed8cc191fa20223
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-06-20 17:45:08 +0100

    Removing test sentry alert after success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::API
 
   # rubocop:disable Metrics/MethodLength
   def authorize_request
-    Sentry.capture_message('Hello Sentry Test')
-
     encrypted_payload = request.raw_post
     return render_unauthorized if encrypted_payload.nil? || encrypted_payload.empty?
 
