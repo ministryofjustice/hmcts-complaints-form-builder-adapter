708c1efb659397d698f24bfa1a9bd30e17da59d6
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-06-21 17:53:02 +0100

    Adding temporary fix to distinguish between formbuilder and mojforms

--- app/lib/presenter/comment.rb
+++ app/lib/presenter/comment.rb
@@ -2,16 +2,30 @@ module Presenter
   class Comment < BasePresenter
     REQUEST_METHOD = 'Online form'.freeze
 
+    # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
     def optics_payload
-      {
-        Type: type,
-        RequestDate: request_date,
-        RequestMethod: REQUEST_METHOD,
-        AssignedTeam: submission_answers.fetch(:contact_location, ''),
-        'Case.ServiceTeam': submission_answers.fetch(:contact_location, ''),
-        Details: submission_answers.fetch(:feedback_details, '')
-      }
+      service_slug = form_builder_payload.fetch(:serviceSlug)
+      if service_slug.include? 'hmcts-comments-form'
+        {
+          Type: type,
+          RequestDate: request_date,
+          RequestMethod: REQUEST_METHOD,
+          AssignedTeam: submission_answers.fetch(:'which-contact-with_autocomplete_1', ''),
+          'Case.ServiceTeam': submission_answers.fetch(:'which-contact-with_autocomplete_1', ''),
+          Details: submission_answers.fetch(:feedback_textarea_1, '')
+        }
+      else
+        {
+          Type: type,
+          RequestDate: request_date,
+          RequestMethod: REQUEST_METHOD,
+          AssignedTeam: submission_answers.fetch(:contact_location, ''),
+          'Case.ServiceTeam': submission_answers.fetch(:contact_location, ''),
+          Details: submission_answers.fetch(:feedback_details, '')
+        }
+      end
     end
+    # rubocop:enable Metrics/MethodLength, Naming/VariableNumber
 
     private
 
--- spec/presenter/comment_spec.rb
+++ spec/presenter/comment_spec.rb
@@ -93,5 +93,28 @@ RSpec.describe Presenter::Comment do
         expect(presenter.optics_payload[:RequestDate]).to eq(today)
       end
     end
+
+    context 'moj forms v2' do
+      let(:base_payload) do
+        {
+          serviceSlug: 'hmcts-comments-form-eng',
+          submissionId: 'df58b73f-a8c2-4c2a-90be-da67d3c24136',
+          submissionAnswers:
+          {
+            RequestMethod: Presenter::Comment::REQUEST_METHOD,
+            "which-contact-with_autocomplete_1": 'Champions',
+            feedback_textarea_1: 'this is new information'
+          }
+        }
+        end
+
+        it 'returns the contact location' do
+          expect(presenter.optics_payload[:AssignedTeam]).to eq('Champions')
+        end
+
+        it 'returns the details' do
+          expect(presenter.optics_payload[:Details]).to eq('this is new information')
+        end
+    end
   end
 end
