6b19cb2b002bdb8a8c03a4d419aa5d96ddf71a5f
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-06-23 16:14:05 +0100

    Refactoring request spec before adding routes for v2 feedback

--- spec/requests/feedback_spec.rb
+++ spec/requests/feedback_spec.rb
@@ -48,9 +48,6 @@ describe 'Submitting feedback', type: :request do
           }
         }
       )
-    perform_enqueued_jobs do
-      post '/v1/feedback', params: encrypted_body(msg: runner_submission)
-    end
   end
 
   let(:expected_optics_payload) do
@@ -66,53 +63,61 @@ describe 'Submitting feedback', type: :request do
     }.to_json
   end
 
-  let(:runner_submission) do
-    {
-      serviceSlug: 'user-feedback',
-      submissionId: '891c837c-adef-4854-8bd0-d681577f381e',
-      submissionAnswers:
-      {
-        contact_location: '1111',
-        feedback_details: 'all of the feedback'
-      }
-    }.to_json
-  end
-
   after do
     Timecop.return
   end
 
-  include_context 'when authentication required' do
-    let(:url) { '/v1/feedback' }
-  end
-
-  it 'returns 201 on a valid post' do
-    expect(response).to have_http_status(:created)
-  end
-
-  describe 'end to end submission' do
-    it 'requests a bearer token' do
-      expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/token?db=hmcts').with(
-        headers: { 'Content-Type' => 'application/x-www-form-urlencoded' },
-        body: 'grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzb21lX29wdGljc19hcGlfa2V5IiwiYXVkIjoiaHR0cHM6Ly91YXQuaWNhc2V3b3JrLmNvbS90b2tlbj9kYj1obWN0cyIsImlhdCI6MTY1MTY3ODQ4Nn0.7f-8HNHcxUK5KV6K5yWUf5C7krkjNANv6it6ADa33FY'
-      ).once
+  context 'legacy formbuilder v1' do
+    before do
+      perform_enqueued_jobs do
+        post '/v1/feedback', params: encrypted_body(msg: runner_submission)
+      end
     end
 
-    it 'posts the submission to Optics' do
-      expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/createcase?db=hmcts').with(
-        headers: {
-          'Authorization' => 'Bearer some_bearer_token',
-          'Content-Type' => 'application/json'
-        },
-        body: expected_optics_payload
-      ).once
+    let(:runner_submission) do
+      {
+        serviceSlug: 'user-feedback',
+        submissionId: '891c837c-adef-4854-8bd0-d681577f381e',
+        submissionAnswers:
+        {
+          contact_location: '1111',
+          feedback_details: 'all of the feedback'
+        }
+      }.to_json
     end
 
-    it 'records that there was a successful submission' do
-      expect(ProcessedSubmission.count).to eq(1)
-      expect(
-        ProcessedSubmission.first.submission_id
-      ).to eq('891c837c-adef-4854-8bd0-d681577f381e')
+    include_context 'when authentication required' do
+      let(:url) { '/v1/feedback' }
+    end
+
+    it 'returns 201 on a valid post' do
+      expect(response).to have_http_status(:created)
+    end
+
+    describe 'end to end submission' do
+      it 'requests a bearer token' do
+        expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/token?db=hmcts').with(
+          headers: { 'Content-Type' => 'application/x-www-form-urlencoded' },
+          body: 'grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzb21lX29wdGljc19hcGlfa2V5IiwiYXVkIjoiaHR0cHM6Ly91YXQuaWNhc2V3b3JrLmNvbS90b2tlbj9kYj1obWN0cyIsImlhdCI6MTY1MTY3ODQ4Nn0.7f-8HNHcxUK5KV6K5yWUf5C7krkjNANv6it6ADa33FY'
+        ).once
+      end
+
+      it 'posts the submission to Optics' do
+        expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/createcase?db=hmcts').with(
+          headers: {
+            'Authorization' => 'Bearer some_bearer_token',
+            'Content-Type' => 'application/json'
+          },
+          body: expected_optics_payload
+        ).once
+      end
+
+      it 'records that there was a successful submission' do
+        expect(ProcessedSubmission.count).to eq(1)
+        expect(
+          ProcessedSubmission.first.submission_id
+        ).to eq('891c837c-adef-4854-8bd0-d681577f381e')
+      end
     end
   end
 end
