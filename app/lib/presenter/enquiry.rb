c7825010b75dfd4538a27491eb18a659faa4892a
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-06 17:26:20 +0100

    Adding Enquiry presenter

---
+++ app/lib/presenter/enquiry.rb
@@ -0,0 +1,22 @@
+module Presenter
+  class Enquiry < BasePresenter
+    REQUEST_METHOD = 'Online form'.freeze
+    TYPE = 'GeneralEnquiry'.freeze
+
+    # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
+    def optics_payload
+      {
+        Type: TYPE,
+        RequestDate: request_date,
+        RequestMethod: REQUEST_METHOD,
+        'Customer.FirstName': submission_answers.fetch(:contactdetails_text_1, ''),
+        'Customer.Surname': submission_answers.fetch(:contactdetails_text_2, ''),
+        Reference: submission_answers.fetch(:'contact-details_text_1', ''),
+        'Customer.Email': submission_answers.fetch(:usercontactemail_email_1, ''),
+        Details: submission_answers.fetch(:area_textarea_1, ''),
+        AssignedTeam: submission_answers.fetch(:courttribunalorservice_autocomplete_1, 'INBOX')
+      }
+    end
+    # rubocop:enable Metrics/MethodLength, Naming/VariableNumber
+  end
+end
