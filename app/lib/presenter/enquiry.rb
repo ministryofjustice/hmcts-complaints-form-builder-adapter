ffe8f5d94ef46cc743e7e6a8aab16a1fd5448258
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-08 15:27:52 +0100

    Removing type as previous one was rejected by OPTICS

--- app/lib/presenter/enquiry.rb
+++ app/lib/presenter/enquiry.rb
@@ -1,7 +1,7 @@
 module Presenter
   class Enquiry < BasePresenter
     REQUEST_METHOD = 'Online form'.freeze
-    TYPE = 'HMCTSGeneralEnquiry'.freeze
+    TYPE = ''.freeze
 
     # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
     def optics_payload
