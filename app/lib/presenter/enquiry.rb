2eba1b905481bffdf13b3dda1f91675696e12911
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-08 15:04:50 +0100

    Setting to Query type for now

--- app/lib/presenter/enquiry.rb
+++ app/lib/presenter/enquiry.rb
@@ -1,7 +1,7 @@
 module Presenter
   class Enquiry < BasePresenter
     REQUEST_METHOD = 'Online form'.freeze
-    TYPE = 'GeneralQuery'.freeze
+    TYPE = 'Query'.freeze
 
     # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
     def optics_payload
