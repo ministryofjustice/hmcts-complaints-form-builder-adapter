4e850d1d83f231a2fa70020dd9784443c5ef5ffa
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-08 13:33:39 +0100

    Updating type as the last one was rejected by OPTICS

--- app/lib/presenter/enquiry.rb
+++ app/lib/presenter/enquiry.rb
@@ -1,7 +1,7 @@
 module Presenter
   class Enquiry < BasePresenter
     REQUEST_METHOD = 'Online form'.freeze
-    TYPE = 'GeneralEnquiry'.freeze
+    TYPE = 'General Enquiry'.freeze
 
     # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
     def optics_payload
