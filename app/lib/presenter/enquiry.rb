e24b1a75277fc770e490513a62ee2bfea2cb204b
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-11 14:09:52 +0100

    Civiva have updated OPTICS and this type should be accepted now

--- app/lib/presenter/enquiry.rb
+++ app/lib/presenter/enquiry.rb
@@ -1,7 +1,7 @@
 module Presenter
   class Enquiry < BasePresenter
     REQUEST_METHOD = 'Online form'.freeze
-    TYPE = 'Complaint'.freeze
+    TYPE = 'HMCTS General enquiry'.freeze
 
     # rubocop:disable Metrics/MethodLength, Naming/VariableNumber
     def optics_payload
