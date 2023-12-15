c5c8aa6153238f98ffdcf23322ff5c2dac932bef
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-12-15 11:49:20 +0000

    Expand coverage to 100

--- spec/controllers/health_controller_spec.rb
+++ spec/controllers/health_controller_spec.rb
@@ -2,16 +2,34 @@ require 'rails_helper'
 
 describe HealthController do
   describe 'GET #show' do
-    before do
-      get :show
-    end
+    context 'healthy' do
+      before do
+        get :show
+      end
 
-    it 'returns 200 OK' do
-      expect(response.status).to eq(200)
-    end
+      it 'returns 200 OK' do
+        expect(response.status).to eq(200)
+      end
 
-    it 'returns the text "healthy"' do
-      expect(response.body).to eq('healthy')
+      it 'returns the text "healthy"' do
+        expect(response.body).to eq('healthy')
+      end
+    end
+  end
+
+  describe 'GET #readiness' do
+    context 'ready' do
+      before do
+        get :readiness
+      end
+
+      it 'returns 200 OK' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the text "ready"' do
+        expect(response.body).to eq('ready')
+      end
     end
   end
 end
