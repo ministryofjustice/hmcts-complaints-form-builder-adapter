dfbfc5702d7d8d9940a1e53d84afea616e0e6347
Jesus Laiz <zheileman@users.noreply.github.com>
2024-04-10 10:37:22 +0100

    Cleanup legacy address code (#638)
    https://trello.com/c/RfKx3plj
    
    After the introduction of the new address component, we implemented some backwards compatible code to support both types of addresses for a while until the complaints form was fully migrated.
    
    Now the form (English and Welsh versions) is migrated, we can delete the backwards compatibility code and streamline the tests.

--- spec/requests/complaint_spec.rb
+++ spec/requests/complaint_spec.rb
@@ -26,12 +26,12 @@ describe 'Submitting a complaint', type: :request do
       'Customer.Surname': '',
       'Customer.Email': '',
       'Customer.Phone': '',
-      Impact: '',
-      ActionRequested: '',
       'Customer.Address': '',
       'Customer.Town': '',
       'Customer.County': '',
       'Customer.Postcode': '',
+      Impact: '',
+      ActionRequested: '',
       'Document1.Name': 'image.png',
       'Document1.MimeType': 'image/png',
       'Document1.URL': 'https://example.com/v1/attachments/e2161d54-92f8-4e10-b3a1-94630c65df3c',
@@ -50,14 +50,22 @@ describe 'Submitting a complaint', type: :request do
         submissionId: '72c49803-e9c3-42ac-bde1-09c04595a2d3',
         submissionAnswers:
         {
-          'yourname_text_1': '',
-          'yourname_text_2': '',
+          'yourname_text_1': 'John',
+          'yourname_text_2': 'Doe',
           'casenumber_text_1': '',
           'youremailaddress_email_1': '',
           'yourcomplaint_textarea_1': '',
           'howhasthisaffectedyou_textarea_1': '',
           'whatcanwedotoputthisright_textarea_1': '',
-          'courtortribunalyourcomplaintisabout_autocomplete_1': '111'
+          'courtortribunalyourcomplaintisabout_autocomplete_1': '111',
+          'youraddress_address_1': {
+            'address_line_one' => 'Street 123',
+            'address_line_two' => 'High Tower',
+            'city' => 'London',
+            'county' => 'Greater London',
+            'postcode' => 'SW1H 9EA',
+            'country' => 'United Kingdom'
+          }
         },
         attachments: [{
           url: 'https://example.com/s3/image.png',
@@ -70,7 +78,7 @@ describe 'Submitting a complaint', type: :request do
     end
 
     let(:payload_body) do
-      "{\"Team\":\"111\",\"AssignedTeam\":\"111\",\"AssignedTeamSS\":\"111\",\"RequestDate\":\"2019-09-11\",\"Details\":\"\",\"Reference\":\"\",\"db\":\"hmcts\",\"Type\":\"Complaint\",\"Format\":\"json\",\"RequestMethod\":\"Online - gov.uk\",\"PartyContextManageCases\":\"Main\",\"Customer.FirstName\":\"\",\"Customer.Surname\":\"\",\"Customer.Email\":\"\",\"Customer.Phone\":\"\",\"Impact\":\"\",\"ActionRequested\":\"\",\"Customer.Address\":\"\",\"Customer.Town\":\"\",\"Customer.County\":\"\",\"Customer.Postcode\":\"\",\"Document1.Name\":\"image.png\",\"Document1.MimeType\":\"image/png\",\"Document1.URL\":\"https://example.com/v1/attachments/e2161d54-92f8-4e10-b3a1-94630c65df3c\",\"Document1.URLLoadContent\":true}"
+      "{\"Team\":\"111\",\"AssignedTeam\":\"111\",\"AssignedTeamSS\":\"111\",\"RequestDate\":\"2019-09-11\",\"Details\":\"\",\"Reference\":\"\",\"db\":\"hmcts\",\"Type\":\"Complaint\",\"Format\":\"json\",\"RequestMethod\":\"Online - gov.uk\",\"PartyContextManageCases\":\"Main\",\"Customer.FirstName\":\"John\",\"Customer.Surname\":\"Doe\",\"Customer.Email\":\"\",\"Customer.Phone\":\"\",\"Customer.Address\":\"Street 123, High Tower\",\"Customer.Town\":\"London\",\"Customer.County\":\"Greater London\",\"Customer.Postcode\":\"SW1H 9EA\",\"Impact\":\"\",\"ActionRequested\":\"\",\"Document1.Name\":\"image.png\",\"Document1.MimeType\":\"image/png\",\"Document1.URL\":\"https://example.com/v1/attachments/e2161d54-92f8-4e10-b3a1-94630c65df3c\",\"Document1.URLLoadContent\":true}"
     end
 
     before do
@@ -103,36 +111,6 @@ describe 'Submitting a complaint', type: :request do
       expect(response).to have_http_status(:created)
     end
 
-    context 'for a submission with containing a new address component' do
-      let(:runner_submission) do
-        super().merge(
-          {
-            submissionAnswers: {
-              'yourname_text_1': 'John',
-              'yourname_text_2': 'Doe',
-              'courtortribunalyourcomplaintisabout_autocomplete_1': '111',
-              'youraddress_address_1': {
-                'address_line_one' => 'Street 123',
-                'address_line_two' =>'High Tower',
-                'city' => 'London',
-                'county' => 'Greater London',
-                'postcode' => 'SW1H 9EA',
-                'country' => 'United Kingdom'
-              }
-            }
-          }
-        )
-      end
-
-      let(:payload_body) do
-        "{\"Team\":\"111\",\"AssignedTeam\":\"111\",\"AssignedTeamSS\":\"111\",\"RequestDate\":\"2019-09-11\",\"Details\":\"\",\"Reference\":\"\",\"db\":\"hmcts\",\"Type\":\"Complaint\",\"Format\":\"json\",\"RequestMethod\":\"Online - gov.uk\",\"PartyContextManageCases\":\"Main\",\"Customer.FirstName\":\"John\",\"Customer.Surname\":\"Doe\",\"Customer.Email\":\"\",\"Customer.Phone\":\"\",\"Impact\":\"\",\"ActionRequested\":\"\",\"Customer.Address\":\"Street 123, High Tower\",\"Customer.Town\":\"London\",\"Customer.County\":\"Greater London\",\"Customer.Postcode\":\"SW1H 9EA\",\"Document1.Name\":\"image.png\",\"Document1.MimeType\":\"image/png\",\"Document1.URL\":\"https://example.com/v1/attachments/e2161d54-92f8-4e10-b3a1-94630c65df3c\",\"Document1.URLLoadContent\":true}"
-      end
-
-      it 'returns 201 on a valid post' do
-        expect(response).to have_http_status(:created)
-      end
-    end
-
     describe 'end to end submission' do
       it 'requests a bearer token' do
         expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/token?db=hmcts').with(
@@ -144,7 +122,7 @@ describe 'Submitting a complaint', type: :request do
       it 'posts the submission to Optics' do
         expect(WebMock).to have_requested(:post, 'https://uat.icasework.com/createcase?db=hmcts').with(
           headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer some_bearer_token', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'},
-          body: expected_optics_payload.to_s
+          body: payload_body
         ).once
       end
 
